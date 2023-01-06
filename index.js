a91283a50ee906d5ed416eedb4ea074f17226455
borisvorobev <boriscvorobev@gmail.com>
2023-01-06 02:09:47 +0000

    added .env.example and login/logout routes

--- index.js
+++ index.js
@@ -3,8 +3,55 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+require("dotenv").config();
+
+const session = require("express-session");
+
+const secret = process.env.SESSION_SECRET;
+const store = new session.MemoryStore();
+const protect = (req, res, next) => {
+  const { authenticated } = req.session;
+
+  if (!authenticated) {
+    res.sendStatus(401);
+  } else {
+    next();
+  }
+};
+
+app.use(
+  session({
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    store,
+  })
+);
+
 app.get("/", (req, res) => {
   const { name = 'user' } = req.query;
+  res.send(`Hello ${name}!`);
+});
+
+app.get("/login", (req, res) => {
+  const { authenticated } = req.session;
+
+  if (!authenticated) {
+    req.session.authenticated = true;
+    res.send("Successfully authenticated");
+  } else {
+    res.send("Already authenticated");
+  }
+});
+
+app.get("/logout", protect, (req, res) => {
+  req.session.destroy(() => {
+    res.send("Successfully logged out");
+  });
+});
+
+app.get("/protected", protect, (req, res) => {
+  const { name = "user" } = req.query;
   res.send(`Hello ${name}!`);
 });
 
